755a2ffff2b5b4b30ec52779703fe1d8e38a40ab
adimidania <ja_adimi@esi.dz>
2021-08-08 18:11:46 +0200

    first commit

---
+++ build/precache-manifest.732bedc90a3077159bd014c763a9f179.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e116399c40e6fe2cb1ee47deb84c90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "264ca025de2acc61bc41",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "de89b5950fb2f69413b4",
+    "url": "/static/css/main.ad60371f.chunk.css"
+  },
+  {
+    "revision": "264ca025de2acc61bc41",
+    "url": "/static/js/2.dffbe55c.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.dffbe55c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de89b5950fb2f69413b4",
+    "url": "/static/js/main.7fda2d43.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
